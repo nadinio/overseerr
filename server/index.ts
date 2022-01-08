4ee7edd9995c9d91fd7d71e16f65b8ba407f8eed
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2022-01-08 13:20:07 -0500

    fix: allow alert to be dismissed

--- server/index.ts
+++ server/index.ts
@@ -28,6 +28,7 @@ import { getSettings } from './lib/settings';
 import logger from './logger';
 import routes from './routes';
 import { getAppVersion } from './utils/appVersion';
+import restartFlag from './utils/restartFlag';
 
 const API_SPEC_PATH = path.join(__dirname, '../overseerr-api.yml');
 
@@ -50,6 +51,7 @@ app
 
     // Load Settings
     const settings = getSettings().load();
+    restartFlag.initializeSettings(settings);
 
     // Migrate library types
     if (
